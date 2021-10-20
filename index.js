d4612063672161324737239739706485190f58c8
ivandi1980 <ivandi.djoh@gmail.com>
2021-10-21 04:32:26 +0800

    add more query

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
-console.log(`Prisma`);
+const { PrismaClient } = require('@prisma/client')
+
+const prisma = new PrismaClient()
+
+async function main() {
+    // const allUsers = await prisma.user.findMany()
+    // console.log(allUsers)
+
+    // await prisma.user.create({
+    //     data: {
+    //         name: 'Ivandi',
+    //         email: 'ivandi@ivandi.io',
+    //         posts: {
+    //             create: { title: 'Hello World' },
+    //         },
+    //         profile: {
+    //             create: { bio: 'I like turtles' },
+    //         },
+    //     },
+    // })
+
+    // const post = await prisma.post.update({
+    //     where: { id: 1 },
+    //     data: { published: true },
+    // })
+    // console.log(post)
+
+    // const allUsers = await prisma.user.findMany({
+    //     include: {
+    //         posts: true,
+    //         profile: true,
+    //     },
+    // })
+
+    const userById = await prisma.user.findUnique({
+        where: {
+            id: 4
+        }
+    })
+    // console.dir(allUsers, { depth: null })
+    console.dir(userById, { depth: null })
+}
+
+main()
+    .catch((e) => {
+        throw e
+    })
+    .finally(async () => {
+        await prisma.$disconnect()
+    })
