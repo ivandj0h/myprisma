2ae8a0055b7a871c91c82e037c71a3b7ec973dc0
ivandi1980 <ivandi.djoh@gmail.com>
2021-10-21 03:59:54 +0800

    create prisma migration

---
+++ prisma/migrations/20211020195831_init/migration.sql
@@ -0,0 +1,51 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `createdAt` on the `user` table. All the data in the column will be lost.
+  - You are about to drop the column `updatedAt` on the `user` table. All the data in the column will be lost.
+  - You are about to drop the column `username` on the `user` table. All the data in the column will be lost.
+  - A unique constraint covering the columns `[email]` on the table `User` will be added. If there are existing duplicate values, this will fail.
+  - Added the required column `email` to the `User` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- DropIndex
+DROP INDEX `User_username_key` ON `user`;
+
+-- AlterTable
+ALTER TABLE `user` DROP COLUMN `createdAt`,
+    DROP COLUMN `updatedAt`,
+    DROP COLUMN `username`,
+    ADD COLUMN `email` VARCHAR(191) NOT NULL,
+    ADD COLUMN `name` VARCHAR(191) NULL;
+
+-- CreateTable
+CREATE TABLE `Post` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `title` VARCHAR(255) NOT NULL,
+    `content` VARCHAR(191) NULL,
+    `published` BOOLEAN NOT NULL DEFAULT false,
+    `authorId` INTEGER NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Profile` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `bio` VARCHAR(191) NULL,
+    `userId` INTEGER NOT NULL,
+
+    UNIQUE INDEX `Profile_userId_key`(`userId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateIndex
+CREATE UNIQUE INDEX `User_email_key` ON `User`(`email`);
+
+-- AddForeignKey
+ALTER TABLE `Post` ADD CONSTRAINT `Post_authorId_fkey` FOREIGN KEY (`authorId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Profile` ADD CONSTRAINT `Profile_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
